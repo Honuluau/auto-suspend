ea990f1484d0635f01cb8f885c1b2a159bce5069
honuluau <honu.dylan@gmail.com>
2026-03-09 08:59:54 -0400

    Updated Logger to Create File

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿public class AutoSuspend
 {
-    static void Main(String[] args)
+    static int Main(String[] args)
     {
+        bool initializedLog = Logger<AutoSuspend>.InitializeLog();
+        if (!initializedLog)
+        {
+            return 1;
+        }
+
         Console.WriteLine("Hello, World!");
         Logger<AutoSuspend>.Log("Program completed line 5 of program.cs", LogLevel.Info);
+        Logger<AutoSuspend>.Log("Program completed line 6 of program.cs", LogLevel.Debug);
+        return 0;
     }
 }
--- Util/Logger.cs
+++ Util/Logger.cs
@@ -1,8 +1,27 @@
 public static class Logger<T>
 {
+    private const string LOG_PATH = "log.txt";
+
     public static void Log(string message, LogLevel level)
     {
         string log = $"[{DateTime.Now}]\t[{level}]\t[{typeof(T).Name}]\t{message}";
-        Console.WriteLine(log);
+        File.AppendAllText(LOG_PATH, log + Environment.NewLine);
+    }
+
+    public static bool InitializeLog()
+    {
+        try
+        {
+            if (!File.Exists(LOG_PATH))
+            {
+                File.Create(LOG_PATH).Close();
+            }
+            return true;
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"An error occured initializing log: {e.Message}");
+            return false;
+        }
     }
 }
