7d7cfd95b6206817657491184c6f8aae0a4133db
honuluau <honu.dylan@gmail.com>
2026-04-07 09:20:21 -0400

    Removed alma_id from Patron sql class.

--- Modules/Sql/Classes/Patron.cs
+++ Modules/Sql/Classes/Patron.cs
@@ -1,7 +1,6 @@
 public class Patron
 {
     public int id { get; set; }
-    public string alma_id { get; set; }
     public string eagle_id { get; set; }
     public string first_name { get; set; }
     public string last_name { get; set; }
