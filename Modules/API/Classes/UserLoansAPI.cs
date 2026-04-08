431af95946fe6e6b4d411d3cac13b39fdb526208
honuluau <honu.dylan@gmail.com>
2026-04-08 09:42:58 -0400

    Updated UserLoans API to include item information gathered from the the Loan request.

--- Modules/API/Classes/UserLoansAPI.cs
+++ Modules/API/Classes/UserLoansAPI.cs
@@ -4,14 +4,27 @@ public class CircDeskAPI
     public string? desc { get; set; }
 }
 
+public class ItemPolicyAPI
+{
+    public string? value { get; set; }
+    public string? desc { get; set; }
+}
+
 public class LoanAPI
 {
     public required string loan_id { get; set; }
     public required CircDeskAPI circ_desk { get; set; }
     public required string user_id { get; set; }
-    public required string mms_id { get; set; }
     public required string due_date { get; set; }
     public required string loan_date { get; set; }
+
+    // item related
+    public required string item_barcode { get; set; }
+    public required string mms_id { get; set; }
+    public required string title { get; set; }
+    public required string description { get; set; }
+    public required ItemPolicyAPI item_policy { get; set; }
+
     public CircDeskAPI? return_circ_desk { get; set; }
 }
 
