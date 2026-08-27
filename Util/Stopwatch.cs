36376ac56a8c772bd7552c6a89abad293b4be9c3
honuluau <honu.dylan@gmail.com>
2026-08-27 14:59:32 -0400

    Finalized Stopwatch.cs

--- Util/Stopwatch.cs
+++ Util/Stopwatch.cs
@@ -3,6 +3,10 @@ public class Stopwatch
     private static DateTime? start = null;
     private static DateTime? end = null;
 
+    /// <summary>
+    /// This method starts the stop watch by saving DateTime.now to a static property.
+    /// </summary>
+    /// <returns>The string format of DateTime.now.</returns>
     public static string Start()
     {
         start = DateTime.Now;
@@ -10,6 +14,10 @@ public class Stopwatch
         return start.ToString()!;
     }
 
+    /// <summary>
+    /// This stops the stopwatch.
+    /// </summary>
+    /// <returns>Returns the length of the time between start and stop.</returns>
     public static string Stop()
     {
         end = DateTime.Now;
