0a854e49116e1ec4487fd5c8fe37a7673eab2c61
honuluau <honu.dylan@gmail.com>
2026-08-27 17:26:38 -0400

    Finalized HttpClientHouse

--- Util/HttpClientHouse.cs
+++ Util/HttpClientHouse.cs
@@ -1,14 +1,19 @@
-public class HttpClientHouse
-{
+public class HttpClientHouse {
     private static HttpClient httpClient = new HttpClient();
 
-    public static void SetHttpClient(HttpClient newHttpClient)
-    {
-        httpClient = newHttpClient;
+    /// <summary>
+    /// Retrieves static HttpClient.
+    /// </summary>
+    /// <returns>Static HttpClient among all classes.</returns>
+    public static HttpClient GetHttpClient() {
+        return httpClient;
     }
 
-    public static HttpClient GetHttpClient()
-    {
-        return httpClient;
+    /// <summary>
+    /// This method is only called once by Auto-Suspend and it updates the HttpClient.
+    /// </summary>
+    /// <param name="newHttpClient"></param>
+    public static void SetHttpClient(HttpClient newHttpClient) {
+        httpClient = newHttpClient;
     }
 }
