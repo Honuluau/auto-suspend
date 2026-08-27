d3ecec269763ccda94d640869a6ab007f63a5270
honuluau <honu.dylan@gmail.com>
2026-08-27 17:22:54 -0400

    Finalized MathUtil

--- Util/MathUtil.cs
+++ Util/MathUtil.cs
@@ -1,6 +1,12 @@
 public class MathUtil
 {
-    // Credits to Trap (7839) on StackOverflow. 
+    /// <summary>
+    /// A quick Clamp method that I got from Trap (userid: 7839) on StackOverflow.
+    /// </summary>
+    /// <param name="value">Value to be clamped.</param>
+    /// <param name="min">Minimum value.</param>
+    /// <param name="max">Maximum Value.</param>
+    /// <returns>Value within range.</returns>
     public static int Clamp(int value, int min, int max)
     {
         return (value < min) ? min : (value > max) ? max : value;
