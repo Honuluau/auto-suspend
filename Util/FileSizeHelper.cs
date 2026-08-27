b2a56fb4c4e67675510124d28a8aa637461d2d31
honuluau <honu.dylan@gmail.com>
2026-08-27 17:24:47 -0400

    Finalized FIleSizeHelper

--- Util/FileSizeHelper.cs
+++ Util/FileSizeHelper.cs
@@ -1,13 +1,16 @@
-public class FileSizeHelper
-{
+public class FileSizeHelper {
     private static readonly string[] Units = { "B", "KB", "MB", "GB", "TB", "PB" };
-    public static string GetReadableFileSize(long size)
-    {
+
+    /// <summary>
+    /// Formats a byte number by it's relative suffix.
+    /// </summary>
+    /// <param name="size">File size in bytes.</param>
+    /// <returns>A string that is the amount + suffix.</returns>
+    public static string GetReadableFileSize(long size) {
         int unitIndex = 0;
         double readableSize = (double)size;
 
-        while (readableSize >= 1024 && unitIndex < Units.Length - 1)
-        {
+        while (readableSize >= 1024 && unitIndex < Units.Length - 1) {
             readableSize /= 1024;
             unitIndex++;
         }
