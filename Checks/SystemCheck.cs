39ed4f7b61d693f3f17af7d6e8bee47572a49f56
honuluau <honu.dylan@gmail.com>
2026-08-31 11:17:29 -0400

    Finalized SystemCheck.cs

--- Checks/SystemCheck.cs
+++ Checks/SystemCheck.cs
@@ -2,38 +2,17 @@ using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
-public class SystemCheck
-{
+public class SystemCheck {
     public static bool online { get; set; }
     public static bool availableStorage { get; set; }
     public static bool directories { get; set; }
     public static bool files { get; set; }
 
-    public static async Task<bool> CheckInternetConnection(HttpClient httpClient)
-    {
-        try
-        {
-            HttpResponseMessage response = await httpClient.GetAsync("http://www.google.com");
-
-            if (response.IsSuccessStatusCode)
-            {
-                return true;
-            }
-            else
-            {
-                Logger<SystemCheck>.Log("No internet.", LogLevel.Error);
-                return false;
-            }
-        }
-        catch (Exception e)
-        {
-            Logger<SystemCheck>.Error("No internet", e);
-            return false;
-        }
-    }
-
-    public static int CheckAvailableStorage()
-    {
+    /// <summary>
+    /// This method checks to see if there is at least 1 MB of storage of the current Auto-Suspend directory.
+    /// </summary>
+    /// <returns>Integer overflow.</returns>
+    public static int CheckAvailableStorage() {
         string currentDirectory = Directory.GetCurrentDirectory()!;
         DriveInfo drive = new DriveInfo(Path.GetPathRoot(currentDirectory)!);
         long availableFreeSpace = drive.AvailableFreeSpace;
@@ -43,25 +22,25 @@ public class SystemCheck
             Logger<SystemCheck>.Log($"{drive.Name} has {FileSizeHelper.GetReadableFileSize(availableFreeSpace)} of storage which is less than 1MB.", LogLevel.Error);
             return 2;
         }
-        else
-        {
+        else {
             return 0;
         }
     }
 
-    public static int CheckDirectories(string path)
-    {
+    /// <summary>
+    /// This method ensures that the main Auto-Suspend directory exists by creating if not found.
+    /// </summary>
+    /// <param name="path">Main Auto-Suspend path.</param>
+    /// <returns>Integer overflow.</returns>
+    public static int CheckDirectories(string path) {
         // Main Directory.
-        if (!Directory.Exists(path))
-        {
+        if (!Directory.Exists(path)) {
             Logger<SystemCheck>.Log("Main directory not found.", LogLevel.Info);
-            try
-            {
+            try {
                 Directory.CreateDirectory(path);
                 Logger<SystemCheck>.Log("Created main directory.", LogLevel.Info);
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Logger<SystemCheck>.Error("Main directory unable to be created", e);
                 return 4;
             }
@@ -70,54 +49,80 @@ public class SystemCheck
         return 0;
     }
 
-    public static int CheckFiles(String path)
-    {
+    /// <summary>
+    /// This method checks to see if important files exist and create them if they are not found.
+    /// </summary>
+    /// <param name="path">Main Auto-Suspend path.</param>
+    /// <returns>Integer overflow.</returns>
+    public static int CheckFiles(String path) {
         int exitCode = 0;
 
         // Config file.
         string configFilePath = path + "config.json";
-        if (!File.Exists(configFilePath))
-        {
+        if (!File.Exists(configFilePath)) {
             Logger<SystemCheck>.Log("Config file not found", LogLevel.Info);
             exitCode = Config.CreateConfig(configFilePath);
         }
-        else
-        {
+        else {
             exitCode = Config.InitializeConfig(configFilePath);
         }
-        if (exitCode != 0)
-        {
+        if (exitCode != 0) {
             return exitCode;
         }
 
         return exitCode;
     }
 
-    public static async Task<int> CheckSystem(string path)
-    {
+    /// <summary>
+    /// This method checks the internet connection by requesting google.com. Google.com should be reliable
+    /// enough to check.
+    /// </summary>
+    /// <param name="httpClient">Standard httpClient that Auto-Suspend holds in httpClientHouse.</param>
+    /// <returns>Integer overflow.</returns>
+    public static async Task<bool> CheckInternetConnection(HttpClient httpClient) {
+        try {
+            HttpResponseMessage response = await httpClient.GetAsync("http://www.google.com");
+
+            if (response.IsSuccessStatusCode) {
+                return true;
+            }
+            else {
+                Logger<SystemCheck>.Log("No internet.", LogLevel.Error);
+                return false;
+            }
+        }
+        catch (Exception e) {
+            Logger<SystemCheck>.Error("No internet", e);
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// This method is the overall check system method that executes more specific methods such as:
+    /// Check Internet Connection, Available Storage, Directories, and Files.
+    /// </summary>
+    /// <param name="path">Main Auto-Suspend path.</param>
+    /// <returns>Integer overflow</returns>
+    public static async Task<int> CheckSystem(string path) {
         HttpClient httpClient = HttpClientHouse.GetHttpClient();
 
         bool online = await CheckInternetConnection(httpClient);
-        if (!online)
-        {
+        if (!online) {
             return 2;
         }
 
         int availableStorage = CheckAvailableStorage();
-        if (availableStorage != 0)
-        {
+        if (availableStorage != 0) {
             return availableStorage;
         }
 
         int directories = CheckDirectories(path);
-        if (directories != 0)
-        {
+        if (directories != 0) {
             return directories;
         }
 
         int checkFiles = CheckFiles(path);
-        if (checkFiles != 0)
-        {
+        if (checkFiles != 0) {
             return checkFiles;
         }
 
