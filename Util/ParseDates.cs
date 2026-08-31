bac5ab10c93e6114446e242bc346258c769ec662
honuluau <honu.dylan@gmail.com>
2026-08-31 10:54:53 -0400

    Finalized ParseDates.cs

--- Util/ParseDates.cs
+++ Util/ParseDates.cs
@@ -1,17 +1,35 @@
 public class ParseDates
 {
-    public static DateTime ConvertStringToDateTime(string dateTimeString)
-    {
-        return DateTime.Parse(dateTimeString, null, System.Globalization.DateTimeStyles.RoundtripKind);
-    }
-
-    public static string TodayYearMonthDay()
-    {
-        return DateTime.Today.ToString("yyyy-MM-dd");
-    }
-
+    /// <summary>
+    /// Turns datestring into an american format: Aug 27, 2026
+    /// </summary>
+    /// <param name="dateTime">DateTime</param>
+    /// <returns>A string formatted as such: "Aug 27, 2026"</returns>
     public static string AmericanFormat(DateTime dateTime)
     {
         return dateTime.ToString("MMM dd, yyyy");
+    }
+
+    /// <summary>
+    /// Convertings a string into a datetime.
+    /// </summary>
+    /// <param name="dateTimeString">A string that matches DateTimeStyle: RoundtripKind</param>
+    /// <returns>A DateTime object or null.</returns>
+    public static DateTime? ConvertStringToDateTime(string dateTimeString)
+    {
+        try {
+            return DateTime.Parse(dateTimeString, null, System.Globalization.DateTimeStyles.RoundtripKind);        
+        } catch (Exception) {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// This method gets the current date and sets it to the following format: 2026-08-27. August 27th, 2026.
+    /// </summary>
+    /// <returns>Returns the current day as the following format: 2026-08-27. August 27th, 2026</returns>
+    public static string TodayYearMonthDay()
+    {
+        return DateTime.Today.ToString("yyyy-MM-dd");
     }
 }
