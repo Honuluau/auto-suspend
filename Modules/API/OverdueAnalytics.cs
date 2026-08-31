92fc84114b45936209b9526fe1f4987583330563
honuluau <honu.dylan@gmail.com>
2026-08-31 11:33:14 -0400

    Finalized Overdue Analytics.cs

--- Modules/API/OverdueAnalytics.cs
+++ Modules/API/OverdueAnalytics.cs
@@ -46,45 +46,71 @@ public class Overdue // Derived information from the custom fulfillment report c
 
 public class OverdueAnalytics
 {
-    private static XNamespace ROWSET = "urn:schemas-microsoft-com:xml-analysis:rowset";
-    private static int totalRequests = 0;
-
     public static List<Overdue> Overdues = new List<Overdue>();
 
-    private static int InsertOverdue(Overdue overdue)
+    private static readonly XNamespace ROWSET = "urn:schemas-microsoft-com:xml-analysis:rowset";
+    private static int _totalRequests = 0;
+
+    /// <summary>
+    /// This method gathers all the loans that are listed in a custom fulfillment report created by Justin.
+    /// And inserts them into the SQL Database.
+    /// </summary>
+    /// <returns>Integer overflow.</returns>
+    public static async Task<int> GatherOverdueAnalytics()
     {
-        try
+        HttpClient httpClient = HttpClientHouse.GetHttpClient();
+        string? resumptionToken = null;
+
+        Logger<OverdueAnalytics>.Log($"Reading overdue analytics through API and writing to database.", LogLevel.Info);
+        Stopwatch.Start();
+
+        // Get pages of overdues by iterating with the resumption token.
+        while (true)
         {
-            // Insert Patron
-            SQLInterface.InsertData("patron", ["user_primary_identifier", "first_name", "last_name", "user_group"], [overdue.UserPrimaryIdentifier, overdue.FirstName, overdue.LastName, overdue.UserGroup], 0);
-
-            // Insert Item
-            SQLInterface.InsertData("item", ["barcode", "title"], [overdue.Barcode, overdue.Title], 0);
-
-            // Insert Loan
-            int patronId = SQLInterface.GetIdFromTable("patron", "user_primary_identifier", overdue.UserPrimaryIdentifier);
-            int itemId = SQLInterface.GetIdFromTable("item", "barcode", overdue.Barcode);
-
-            if (patronId > 0 && itemId > 0)
+            string? newResumptionToken = await GetPageOfOverdues(httpClient, resumptionToken);
+            if (newResumptionToken == null)
             {
-                SQLInterface.InsertData("loan", ["alma_id", "out_circ_desk", "patron_id", "item_id", "policy", "preferred_email", "loan_date", "due_date"], [
-                    overdue.ItemLoanId, overdue.CircDesk, patronId, itemId, overdue.ItemPolicy, overdue.PreferredEmail, overdue.LoanDate, overdue.DueDate], 0);
+                break;
+            }
+            else if (newResumptionToken == "Fail")
+            {
+                return 13;
             }
             else
             {
-                Logger<OverdueAnalytics>.Log($"Could not insert overdue because patron or item id is not greater than 0.\npatronId:\t{patronId}\nitemId:\t{itemId}", LogLevel.Error);
-                return 23;
+                resumptionToken = newResumptionToken;
             }
+        }
 
-            return 0;
-        }
-        catch (Exception e)
+        foreach (Overdue overdue in Overdues)
         {
-            Logger<OverdueAnalytics>.Error("Failed to insert overdue", e);
-            return 23;
+            int success = InsertOverdue(overdue);
+            if (success != 0)
+            {
+                return success;
+            }
         }
+
+        Logger<OverdueAnalytics>.Log($"Successfully gathered ({Overdues.Count}) overdue analytics in {Stopwatch.Stop()}.", LogLevel.Info);
+        return 0;
     }
 
+    /// <summary>
+    /// Get method.
+    /// </summary>
+    /// <returns>Number of total requests made with the API endpoint.</returns>
+    public static int GetTotalRequests()
+    {
+        return _totalRequests;
+    }
+
+    /// <summary>
+    /// This method uses an API endpoint to get a page of overdues from a custom fulfillment report created
+    /// by Justin.
+    /// </summary>
+    /// <param name="httpClient">HttpClient to interact with the internet.</param>
+    /// <param name="resumptionToken">A possible token for the next page. If none, then it is done.</param>
+    /// <returns>Possible resumption token.</returns>
     private static async Task<string?> GetPageOfOverdues(HttpClient httpClient, string? resumptionToken)
     {
         string? newResumptionToken = null; // Default to no resumption.
@@ -100,7 +126,7 @@ public class OverdueAnalytics
         {
             // Retrieve XML data
             string xmlData = await httpClient.GetStringAsync(url);
-            totalRequests++;
+            _totalRequests++;
 
             XDocument document = XDocument.Parse(xmlData);
 
@@ -146,47 +172,42 @@ public class OverdueAnalytics
         return newResumptionToken;
     }
 
-    public static async Task<int> GatherOverdueAnalytics()
+    /// <summary>
+    /// This method interfaces with SQLInterface to insert the data of an overdue into the database.
+    /// </summary>
+    /// <param name="overdue">Overdue class that holds all of the information of an overdue loan.</param>
+    /// <returns>Integer overflow.</returns>
+    private static int InsertOverdue(Overdue overdue)
     {
-        HttpClient httpClient = HttpClientHouse.GetHttpClient();
-        string? resumptionToken = null;
-
-        Logger<OverdueAnalytics>.Log($"Reading overdue analytics through API and writing to database.", LogLevel.Info);
-        Stopwatch.Start();
-
-        // Get pages of overdues by iterating with the resumption token.
-        while (true)
+        try
         {
-            string? newResumptionToken = await GetPageOfOverdues(httpClient, resumptionToken);
-            if (newResumptionToken == null)
+            // Insert Patron
+            SQLInterface.InsertData("patron", ["user_primary_identifier", "first_name", "last_name", "user_group"], [overdue.UserPrimaryIdentifier, overdue.FirstName, overdue.LastName, overdue.UserGroup], 0);
+
+            // Insert Item
+            SQLInterface.InsertData("item", ["barcode", "title"], [overdue.Barcode, overdue.Title], 0);
+
+            // Insert Loan
+            int patronId = SQLInterface.GetIdFromTable("patron", "user_primary_identifier", overdue.UserPrimaryIdentifier);
+            int itemId = SQLInterface.GetIdFromTable("item", "barcode", overdue.Barcode);
+
+            if (patronId > 0 && itemId > 0)
             {
-                break;
-            }
-            else if (newResumptionToken == "Fail")
-            {
-                return 13;
+                SQLInterface.InsertData("loan", ["alma_id", "out_circ_desk", "patron_id", "item_id", "policy", "preferred_email", "loan_date", "due_date"], [
+                    overdue.ItemLoanId, overdue.CircDesk, patronId, itemId, overdue.ItemPolicy, overdue.PreferredEmail, overdue.LoanDate, overdue.DueDate], 0);
             }
             else
             {
-                resumptionToken = newResumptionToken;
+                Logger<OverdueAnalytics>.Log($"Could not insert overdue because patron or item id is not greater than 0.\npatronId:\t{patronId}\nitemId:\t{itemId}", LogLevel.Error);
+                return 23;
             }
-        }
 
-        foreach (Overdue overdue in Overdues)
+            return 0;
+        }
+        catch (Exception e)
         {
-            int success = InsertOverdue(overdue);
-            if (success != 0)
-            {
-                return success;
-            }
+            Logger<OverdueAnalytics>.Error("Failed to insert overdue", e);
+            return 23;
         }
-
-        Logger<OverdueAnalytics>.Log($"Successfully gathered ({Overdues.Count}) overdue analytics in {Stopwatch.Stop()}.", LogLevel.Info);
-        return 0;
-    }
-
-    public static int GetTotalRequests()
-    {
-        return totalRequests;
     }
 }
