c2e585287d05f756153ad7aa093c6b29fc963867
honuluau <honu.dylan@gmail.com>
2026-08-31 19:57:14 -0400

    Removed Patron.cs which was an old class that was never utilized because I switched the approach to dynamic json objects.

--- Modules/Sql/Classes/Patron.cs
+++
@@ -1,8 +0,0 @@
-public class Patron
-{
-    public int id { get; set; }
-    public string eagle_id { get; set; }
-    public string first_name { get; set; }
-    public string last_name { get; set; }
-    public string user_group { get; set; }
-}
