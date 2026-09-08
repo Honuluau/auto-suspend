3bab82b78273134f262bf1cc6c267b9c8411c402
honuluau <honu.dylan@gmail.com>
2026-09-08 19:44:07 -0400

    Finalized Note Class

--- Modules/Sql/Classes/Note.cs
+++ Modules/Sql/Classes/Note.cs
@@ -1,14 +1,21 @@
-using System.Data.Common;
-
 public class Note {
-    public int Id { get; set; }
-    public int PatronId { get; set; }
     public DateTime Date { get; set; }
-    public StatusType Status { get; set; }
-    public int Updated { get; set; }
+    public int Id { get; set; }
     public int Instance { get; set; }
     public Loan[] Loans { get; set; }
-
+    public int PatronId { get; set; }
+    public StatusType Status { get; set; }
+    public int Updated { get; set; }
+ 
+    /// <summary>
+    /// This is a constructor method for a Note class in cs.
+    /// </summary>
+    /// <param name="id">Database Id.</param>
+    /// <param name="patronId">Database Id.</param>
+    /// <param name="date">Loan date for loans connected to note.</param>
+    /// <param name="status">Status of the Note.</param>
+    /// <param name="updated">1 = updated in Alma.</param>
+    /// <param name="instance">The incident #; for example: second-offense.</param>
     public Note(int id, int patronId, DateTime date, StatusType status, int updated, int instance) {
         this.Id = id;
         this.PatronId = patronId;
@@ -16,11 +23,15 @@ public class Note {
         this.Status = status;
         this.Updated = updated;
         this.Instance = instance;
+        this.Loans = [];
 
         InitializeLoans();
     }
 
+    /// <summary>
+    /// This method is called upon construction and fills in it's own loans.
+    /// </summary>
     public void InitializeLoans() {
-        this.Loans = SQLInterface.GetLoansForNote(this.Id)!;
+        this.Loans = SQLInterface.GetLoansForNote(this.Id) ?? [];
     }
 }
